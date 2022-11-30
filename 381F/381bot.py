5c92ab477a4fff77d2ba50985ba8b78cbac1bc4a
Desinace <117412592+Desinace@users.noreply.github.com>
2022-11-30 13:39:03 -0600

    Add files via upload

---
+++ 381F/381bot.py
@@ -0,0 +1,95 @@
+# bot.py
+import os
+import VPNpeer as peer
+import GetIP as get
+import schedule
+import time
+import discord
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+oldIP = '172.16.0.2'
+changed = True
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+
+def check():
+    global oldIP
+    global changed
+    global newIP
+    newIP = get.sendIP()
+    if newIP != oldIP:
+        peer.FixPeer(newIP, oldIP)
+        print("The new ip is: "+newIP)
+        changed = False
+        oldIP = newIP
+    else:
+        changed = True
+
+a = """
+type "hi" for a greeting
+type "start" to start continous VPN debugging program
+type "test" to run VPN debugging once
+type "monitor to monitor the interfaces on CSR1
+"""
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        global changed
+        global newIP
+        return
+ 
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+    
+    elif message.content.startswith('start'):
+
+        schedule.every(1).minutes.do(check())
+        if changed == False:
+            await message.channel.send("The new ip address of G0/2 on CSR2 is: " + newIP)
+
+    elif message.content.startswith('test'):
+        await message.channel.send("Testing")
+        check()
+        if changed == False:
+            await message.channel.send("The new ip address of G0/2 on CSR2 is: " + newIP)
+
+    elif message.content.startswith('monitor'):
+        os.system("ansible-playbook -i ./inventory 318ans.yaml")
+        f = open('intMonitor.txt', 'r')
+        rows = []
+        for line in f:
+            row = line.split()
+            rows.append(row)
+        await message.channel.send("CSR2")
+        for i in rows:
+            await message.channel.send("{:<30} {:<30} {:<30} {:<30}".format(*i))
+        f.close()
+        os.system("ansible-playbook -i ./inventory1 318ans.yaml")
+        f = open('intMonitor.txt', 'r')
+        rows1 = []
+        for line in f:
+            row1 = line.split()
+            rows1.append(row1)
+        await message.channel.send("CSR1")
+        for i in rows1:
+            await message.channel.send("{:<30} {:<30} {:<30} {:<30}".format(*i))
+        f.close()
+
+    elif message.content.startswith('help'):
+        await message.channel.send(a)
+        
+    else :
+        await message.channel.send('type "help" for list of commands')
+client.run(TOKEN)
